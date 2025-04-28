c1d71cf3a2a45177514dddbc7e3f2e587d8fbf09
AI-Ned <ta.hill0@googlemail.com>
2025-04-29 00:39:12 +0100

    Fixed bug with ReadSensors calling sensors whilst there still in use.

--- SensorRead.py
+++ SensorRead.py
@@ -1,3 +1,4 @@
+import time
 from ltr559 import LTR559
 from bme280 import BME280
 from smbus2 import SMBus
@@ -11,51 +12,58 @@ class ReadSensors:
     def read_sensor(sensor, do_write:bool):
         bus = SMBus(1)
         bme280 = BME280(i2c_dev=bus)
+        ltr559 = LTR559()
+        ltr559.update_sensor()
+
+        temprature = bme280.get_temperature()
+        pressure = bme280.get_pressure()
+        humidity = bme280.get_humidity()
+        lux = ltr559.get_lux()
+        moisture_sensor1 = Moisture(1).moisture
+        moisture_sensor2 = Moisture(2).moisture
+        moisture_sensor3 = Moisture(3).moisture
 
         if sensor == "temprature" and do_write:
-            DatabaseActions.database_write("bme280_temprature="+str(bme280.get_temperature()))
-            return bme280.get_temperature()
+            DatabaseActions.database_write("bme280_temprature="+str(temprature))
+            return temprature
         elif sensor == "temprature":
-            return bme280.get_temperature()
-        
+            return temprature
         
         if sensor == "pressure" and do_write:
-            DatabaseActions.database_write("bme280_pressure="+str(bme280.get_pressure()))
-            return bme280.get_pressure()
+            DatabaseActions.database_write("bme280_pressure="+str(pressure))
+            return pressure
         elif sensor == "pressure":
-            return bme280.get_pressure()
+            return pressure
         
         if sensor == "humidity" and do_write:
-            DatabaseActions.database_write("bme280_humidity="+str(bme280.get_humidity()))
-            return bme280.get_humidity()
+            DatabaseActions.database_write("bme280_humidity="+str(humidity))
+            return humidity
         elif sensor == "humidity":
-            return bme280.get_humidity()
+            return humidity
 
         if sensor == "light" and do_write:
-            ltr559 = LTR559()
-            ltr559.update_sensor()
-            DatabaseActions.database_write("ltr559_lux="+str(ltr559.get_lux()))
-            return ltr559.get_lux()
+            DatabaseActions.database_write("ltr559_lux="+str(lux))
+            return lux
         elif sensor == "light":
-            return ltr559.get_lux()
+            return lux
         
         if sensor == "moisture1" and do_write:
-            DatabaseActions.database_write("moisture_sensor_1="+str(Moisture(1).moisture))
-            return Moisture(1).moisture
+            DatabaseActions.database_write("moisture_sensor_1="+str(moisture_sensor1))
+            return moisture_sensor1
         elif sensor == "moisture1":
-            return Moisture(1).moisture
+            return moisture_sensor1
         
         if sensor == "moisture2" and do_write:
-            DatabaseActions.database_write("moisture_sensor_2="+str(Moisture(2).moisture))
-            return Moisture(2).moisture
+            DatabaseActions.database_write("moisture_sensor_2="+str(moisture_sensor2))
+            return moisture_sensor2
         elif sensor == "moisture2":
-            return Moisture(2).moisture
+            return moisture_sensor2
         
         if sensor == "moisture3" and do_write:
-            DatabaseActions.database_write("moisture_sensor_3="+str(Moisture(3).moisture))
-            return Moisture(3).moisture
+            DatabaseActions.database_write("moisture_sensor_3="+str(moisture_sensor3))
+            return moisture_sensor3
         elif sensor == "moisture3":
-            return Moisture(3).moisture
+            return moisture_sensor3
 
 
     def read_all(do_write:bool):
