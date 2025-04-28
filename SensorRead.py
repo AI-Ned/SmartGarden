2fcf5aee612923b73210448ea5c2fd5fde7c5c51
AI-Ned <ta.hill0@googlemail.com>
2025-04-29 00:04:17 +0100

    coverted Float values to string to be able concat to data response
    Ignoring the error about RP.GPIO - non issue when deployed to raspi

--- SensorRead.py
+++ SensorRead.py
@@ -13,20 +13,20 @@ class ReadSensors:
         bme280 = BME280(i2c_dev=bus)
 
         if sensor == "temprature" and do_write:
-            DatabaseActions.database_write("bme280_temprature="+bme280.get_temperature())
+            DatabaseActions.database_write("bme280_temprature="+str(bme280.get_temperature()))
             return bme280.get_temperature()
         elif sensor == "temprature":
             return bme280.get_temperature()
         
         
         if sensor == "pressure" and do_write:
-            DatabaseActions.database_write("bme280_pressure="+bme280.get_pressure())
+            DatabaseActions.database_write("bme280_pressure="+str(bme280.get_pressure()))
             return bme280.get_pressure()
         elif sensor == "pressure":
             return bme280.get_pressure()
         
         if sensor == "humidity" and do_write:
-            DatabaseActions.database_write("bme280_humidity="+bme280.get_humidity())
+            DatabaseActions.database_write("bme280_humidity="+str(bme280.get_humidity()))
             return bme280.get_humidity()
         elif sensor == "humidity":
             return bme280.get_humidity()
@@ -34,25 +34,25 @@ class ReadSensors:
         if sensor == "light" and do_write:
             ltr559 = LTR559()
             ltr559.update_sensor()
-            DatabaseActions.database_write("ltr559_lux="+ltr559.get_lux())
+            DatabaseActions.database_write("ltr559_lux="+str(ltr559.get_lux()))
             return ltr559.get_lux()
         elif sensor == "light":
             return ltr559.get_lux()
         
         if sensor == "moisture1" and do_write:
-            DatabaseActions.database_write("moisture_sensor_1="+Moisture(1).moisture)
+            DatabaseActions.database_write("moisture_sensor_1="+str(Moisture(1).moisture))
             return Moisture(1).moisture
         elif sensor == "moisture1":
             return Moisture(1).moisture
         
         if sensor == "moisture2" and do_write:
-            DatabaseActions.database_write("moisture_sensor_2="+Moisture(2).moisture)
+            DatabaseActions.database_write("moisture_sensor_2="+str(Moisture(2).moisture))
             return Moisture(2).moisture
         elif sensor == "moisture2":
             return Moisture(2).moisture
         
         if sensor == "moisture3" and do_write:
-            DatabaseActions.database_write("moisture_sensor_3="+Moisture(3).moisture)
+            DatabaseActions.database_write("moisture_sensor_3="+str(Moisture(3).moisture))
             return Moisture(3).moisture
         elif sensor == "moisture3":
             return Moisture(3).moisture
@@ -63,37 +63,31 @@ class ReadSensors:
         bme280 = BME280(i2c_dev=bus)
         ltr559 = LTR559()
         ltr559.update_sensor()
+
+        sensor_data = {
+                "temprature":str(bme280.get_temperature()),
+                "pressure":str(bme280.get_pressure()),
+                "humidity":str(bme280.get_humidity()),
+                "lux":str(ltr559.get_lux()),
+                "moisture_sensor1":str(Moisture(1).moisture),
+                "moisture_sensor2":str(Moisture(2).moisture),
+                "moisture_sensor3":str(Moisture(3).moisture)
+            }
         
         if do_write:
-            data= "bme280_temprature="+bme280.get_temperature()+ \
-                ",bme280_pressure="+bme280.get_pressure()+ \
-                ",bme280_humidity="+bme280.get_humidity()+ \
-                ",ltr559_lux="+ltr559.get_lux()+ \
-                ",moisture_sensor_1="+Moisture(1).moisture+ \
-                ",moisture_sensor_2="+Moisture(2).moisture+ \
-                ",moisture_sensor_3="+Moisture(3).moisture
+            data= "bme280_temprature="+str(bme280.get_temperature())+ \
+                ",bme280_pressure="+str(bme280.get_pressure())+ \
+                ",bme280_humidity="+str(bme280.get_humidity())+ \
+                ",ltr559_lux="+str(ltr559.get_lux())+ \
+                ",moisture_sensor_1="+str(Moisture(1).moisture)+ \
+                ",moisture_sensor_2="+str(Moisture(2).moisture)+ \
+                ",moisture_sensor_3="+str(Moisture(3).moisture)
             
             DatabaseActions.database_write(data)
 
-            return {
-                "temprature":bme280.get_temperature(),
-                "pressure":bme280.get_pressure(),
-                "humidity":bme280.get_humidity(),
-                "lux":ltr559.get_lux(),
-                "moisture_sensor1":Moisture(1).moisture,
-                "moisture_sensor2":Moisture(2).moisture,
-                "moisture_sensor3":Moisture(3).moisture,
-            }
+            return sensor_data 
         
         else:
-            return {
-                "temprature":bme280.get_temperature(),
-                "pressure":bme280.get_pressure(),
-                "humidity":bme280.get_humidity(),
-                "lux":ltr559.get_lux(),
-                "moisture_sensor1":Moisture(1).moisture,
-                "moisture_sensor2":Moisture(2).moisture,
-                "moisture_sensor3":Moisture(3).moisture,
-            }
+            return sensor_data
 
     
