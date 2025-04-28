1df38e971f3a4cc4a4c5e867ed360fa525d1f4d2
AI-Ned <ta.hill0@googlemail.com>
2025-04-29 00:23:52 +0100

    Update auto_watering.py to read from ReadSensors

--- auto_watering.py
+++ auto_watering.py
@@ -2,13 +2,12 @@ import RPi.GPIO as GPIO # type: ignore
 import yaml
 import time
 from grow.pump import Pump
-from grow.moisture import Moisture
 from SensorRead import ReadSensors
 
 
-moist_sensor1 = Moisture(1)
-moist_sensor2 = Moisture(2)
-moist_sensor3 = Moisture(3)
+moist_sensor1 = ReadSensors.read_sensor("moisture1",0)
+moist_sensor2 = ReadSensors.read_sensor("moisture2",0)
+moist_sensor3 = ReadSensors.read_sensor("moisture3",0)
 pump1 = Pump(1)
 pump2 = Pump(2)
 pump3 = Pump(3)
@@ -28,18 +27,18 @@ WET_POINT = MOISTURE_SETTINGS["wet_point"]
 
 def need_water():
 
-    print(f"Sensor 1 moisture content = {moist_sensor1.moisture}")
-    if moist_sensor1.moisture > WET_POINT:
+    print(f"Sensor 1 moisture content = {moist_sensor1}")
+    if moist_sensor1 > WET_POINT:
         pump1.dose(DOSE_SPEED, DOSE_TIME+0.4)
         time.sleep(5.0)
 
-    print(f"Sensor 2 moisture content = {moist_sensor2.moisture}")
-    if moist_sensor2.moisture > WET_POINT:
+    print(f"Sensor 2 moisture content = {moist_sensor2}")
+    if moist_sensor2 > WET_POINT:
         pump2.dose(DOSE_SPEED, DOSE_TIME)
         time.sleep(5.0)
 
-    print(f"Sensor 3 moisture content = {moist_sensor3.moisture}")
-    if moist_sensor3.moisture > WET_POINT:
+    print(f"Sensor 3 moisture content = {moist_sensor3}")
+    if moist_sensor3 > WET_POINT:
         pump3.dose(DOSE_SPEED, DOSE_TIME+0.6)
         time.sleep(5.0)
 
