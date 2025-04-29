59b19da8e9d0deba5571b2b9430647ac1d0bc72f
AI-Ned <67223760+AI-Ned@users.noreply.github.com>
2025-04-29 23:32:08 +0100

    Development - Read Sensors and Write to database.  (#20)
    * coverted Float values to string to be able concat to data response
    
    Ignoring the error about RP.GPIO - non issue when deployed to raspi
    
    * Update auto_watering.py to read from ReadSensors
    
    * Fixed bug with ReadSensors calling sensors whilst there still in use.
    
    * corrected bugs:
    - SensorRead now reads sensors when requests.
    - database_actions now sends data to database, made table name lowercase due to api limitation
    - Fixed the issue where GPIO wouldn't release.
    
    * fixed bug with sensors not updating correctly.

--- SensorRead.py
+++ SensorRead.py
@@ -1,99 +1,109 @@
+import RPi.GPIO as GPIO # type: ignore
 from ltr559 import LTR559
 from bme280 import BME280
 from smbus2 import SMBus
 from grow.moisture import Moisture
 from database_actions import DatabaseActions
 
+from ltr559 import LTR559
+from bme280 import BME280
+from smbus2 import SMBus
+from grow.moisture import Moisture
+
+
+GPIO.setmode(GPIO.BCM)
+
+bus = SMBus(1)
+bme280 = BME280(i2c_dev=bus)
+ltr559 = LTR559()
+ltr559.update_sensor()
+moisture_sensor1 = Moisture(1)
+moisture_sensor2 = Moisture(2)
+moisture_sensor3 = Moisture(3)
 
 
 class ReadSensors:
 
     def read_sensor(sensor, do_write:bool):
-        bus = SMBus(1)
-        bme280 = BME280(i2c_dev=bus)
 
         if sensor == "temprature" and do_write:
-            DatabaseActions.database_write("bme280_temprature="+bme280.get_temperature())
-            return bme280.get_temperature()
+            temprature = bme280.get_temperature()
+            DatabaseActions.database_write("bme280_temprature="+str(temprature))
+            return temprature
         elif sensor == "temprature":
-            return bme280.get_temperature()
-        
+            temprature = bme280.get_temperature()
+            return temprature
         
         if sensor == "pressure" and do_write:
-            DatabaseActions.database_write("bme280_pressure="+bme280.get_pressure())
-            return bme280.get_pressure()
+            pressure = bme280.get_pressure()
+            DatabaseActions.database_write("bme280_pressure="+str(pressure))
+            return pressure
         elif sensor == "pressure":
-            return bme280.get_pressure()
+            pressure = bme280.get_pressure()
+            return pressure
         
         if sensor == "humidity" and do_write:
-            DatabaseActions.database_write("bme280_humidity="+bme280.get_humidity())
-            return bme280.get_humidity()
+            humidity = bme280.get_humidity()
+            DatabaseActions.database_write("bme280_humidity="+str(humidity))
+            return humidity
         elif sensor == "humidity":
-            return bme280.get_humidity()
+            humidity = bme280.get_humidity()
+            return humidity
 
         if sensor == "light" and do_write:
-            ltr559 = LTR559()
             ltr559.update_sensor()
-            DatabaseActions.database_write("ltr559_lux="+ltr559.get_lux())
-            return ltr559.get_lux()
+            lux = ltr559.get_lux()
+            DatabaseActions.database_write("ltr559_lux="+str(lux))
+            return lux
         elif sensor == "light":
-            return ltr559.get_lux()
+            ltr559.update_sensor()
+            lux = ltr559.get_lux()
+            return lux
         
         if sensor == "moisture1" and do_write:
-            DatabaseActions.database_write("moisture_sensor_1="+Moisture(1).moisture)
-            return Moisture(1).moisture
+            DatabaseActions.database_write("moisture_sensor_1="+str(moisture_sensor1.moisture))
+            return moisture_sensor1.moisture
         elif sensor == "moisture1":
-            return Moisture(1).moisture
+            return moisture_sensor1.moisture
         
         if sensor == "moisture2" and do_write:
-            DatabaseActions.database_write("moisture_sensor_2="+Moisture(2).moisture)
-            return Moisture(2).moisture
+            DatabaseActions.database_write("moisture_sensor_2="+str(moisture_sensor2.moisture))
+            return moisture_sensor2.moisture
         elif sensor == "moisture2":
-            return Moisture(2).moisture
+            return moisture_sensor2.moisture
         
         if sensor == "moisture3" and do_write:
-            DatabaseActions.database_write("moisture_sensor_3="+Moisture(3).moisture)
-            return Moisture(3).moisture
+            DatabaseActions.database_write("moisture_sensor_3="+str(moisture_sensor3.moisture))
+            return moisture_sensor3.moisture
         elif sensor == "moisture3":
-            return Moisture(3).moisture
+            return moisture_sensor3.moisture
+        
+
 
 
     def read_all(do_write:bool):
-        bus = SMBus(1)
-        bme280 = BME280(i2c_dev=bus)
-        ltr559 = LTR559()
-        ltr559.update_sensor()
+
+        sensor_data = {
+                "temprature":str(bme280.get_temperature()),
+                "pressure":str(bme280.get_pressure()),
+                "humidity":str(bme280.get_humidity()),
+                "lux":str(ltr559.get_lux()),
+                "moisture_sensor1":str(moisture_sensor1.moisture),
+                "moisture_sensor2":str(moisture_sensor2.moisture),
+                "moisture_sensor3":str(moisture_sensor3.moisture)
+            }
         
         if do_write:
-            data= "bme280_temprature="+bme280.get_temperature()+ \
-                ",bme280_pressure="+bme280.get_pressure()+ \
-                ",bme280_humidity="+bme280.get_humidity()+ \
-                ",ltr559_lux="+ltr559.get_lux()+ \
-                ",moisture_sensor_1="+Moisture(1).moisture+ \
-                ",moisture_sensor_2="+Moisture(2).moisture+ \
-                ",moisture_sensor_3="+Moisture(3).moisture
+            data= "bme280_temprature="+str(bme280.get_temperature())+ \
+                ",bme280_pressure="+str(bme280.get_pressure())+ \
+                ",bme280_humidity="+str(bme280.get_humidity())+ \
+                ",ltr559_lux="+str(ltr559.get_lux())+ \
+                ",moisture_sensor_1="+str(moisture_sensor1.moisture)+ \
+                ",moisture_sensor_2="+str(moisture_sensor2.moisture)+ \
+                ",moisture_sensor_3="+str(moisture_sensor3.moisture)
             
             DatabaseActions.database_write(data)
 
-            return {
-                "temprature":bme280.get_temperature(),
-                "pressure":bme280.get_pressure(),
-                "humidity":bme280.get_humidity(),
-                "lux":ltr559.get_lux(),
-                "moisture_sensor1":Moisture(1).moisture,
-                "moisture_sensor2":Moisture(2).moisture,
-                "moisture_sensor3":Moisture(3).moisture,
-            }
-        
-        else:
-            return {
-                "temprature":bme280.get_temperature(),
-                "pressure":bme280.get_pressure(),
-                "humidity":bme280.get_humidity(),
-                "lux":ltr559.get_lux(),
-                "moisture_sensor1":Moisture(1).moisture,
-                "moisture_sensor2":Moisture(2).moisture,
-                "moisture_sensor3":Moisture(3).moisture,
-            }
+        return sensor_data
 
     
--- auto_watering.py
+++ auto_watering.py
@@ -1,14 +1,9 @@
-import RPi.GPIO as GPIO
 import yaml
 import time
 from grow.pump import Pump
-from grow.moisture import Moisture
 from SensorRead import ReadSensors
 
 
-moist_sensor1 = Moisture(1)
-moist_sensor2 = Moisture(2)
-moist_sensor3 = Moisture(3)
 pump1 = Pump(1)
 pump2 = Pump(2)
 pump3 = Pump(3)
@@ -26,27 +21,35 @@ DOSE_TIME = PUMP_SETTINGS["dose_time"]
 DRY_POINT = MOISTURE_SETTINGS["dry_point"]
 WET_POINT = MOISTURE_SETTINGS["wet_point"]
 
+watering = 0
+
 def need_water():
 
-    print(f"Sensor 1 moisture content = {moist_sensor1.moisture}")
-    if moist_sensor1.moisture > WET_POINT:
+    moist_sensor1 = ReadSensors.read_sensor("moisture1",0)
+    moist_sensor2 = ReadSensors.read_sensor("moisture2",0)
+    moist_sensor3 = ReadSensors.read_sensor("moisture3",0)
+
+
+    #print(f"Sensor 1 moisture content = {moist_sensor1}")
+    if moist_sensor1 > WET_POINT:
         pump1.dose(DOSE_SPEED, DOSE_TIME+0.4)
-        time.sleep(5.0)
 
-    print(f"Sensor 2 moisture content = {moist_sensor2.moisture}")
-    if moist_sensor2.moisture > WET_POINT:
+    #print(f"Sensor 2 moisture content = {moist_sensor2}")
+    if moist_sensor2 > WET_POINT:
         pump2.dose(DOSE_SPEED, DOSE_TIME)
-        time.sleep(5.0)
 
-    print(f"Sensor 3 moisture content = {moist_sensor3.moisture}")
-    if moist_sensor3.moisture > WET_POINT:
+    #print(f"Sensor 3 moisture content = {moist_sensor3}")
+    if moist_sensor3 > WET_POINT:
         pump3.dose(DOSE_SPEED, DOSE_TIME+0.6)
-        time.sleep(5.0)
 
-    return
 
 
 while True:
-    need_water()
-    ReadSensors.read_all(1)
-    time.sleep(60)
+
+        need_water()
+        time.sleep(1)
+        ReadSensors.read_all(1)
+        time.sleep(300)
+
+    
+    
