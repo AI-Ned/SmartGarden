fbaae18c107c408c3326a9f9df811dec9059afa7
AI-Ned <ta.hill0@googlemail.com>
2025-04-30 00:53:41 +0100

    Added fine calibration to the settings.yml for each pump.
    Updated the auto_watering to all be handled within one for loop.

--- auto_watering.py
+++ auto_watering.py
@@ -3,11 +3,10 @@ import time
 from grow.pump import Pump
 from SensorRead import ReadSensors
 
-
 pump1 = Pump(1)
 pump2 = Pump(2)
 pump3 = Pump(3)
-
+pumps = [pump1,pump2,pump3]
 
 with open('./settings.yml', 'r') as f:
     settings = yaml.load(f, Loader=yaml.SafeLoader)
@@ -21,35 +20,41 @@ DOSE_TIME = PUMP_SETTINGS["dose_time"]
 DRY_POINT = MOISTURE_SETTINGS["dry_point"]
 WET_POINT = MOISTURE_SETTINGS["wet_point"]
 
-watering = 0
-
+#automatically water each plant if the plant requires water. 
 def need_water():
 
-    moist_sensor1 = ReadSensors.read_sensor("moisture1",0)
-    moist_sensor2 = ReadSensors.read_sensor("moisture2",0)
-    moist_sensor3 = ReadSensors.read_sensor("moisture3",0)
+    moist_sensor = [ReadSensors.read_sensor("moisture1",0),ReadSensors.read_sensor("moisture2",0),ReadSensors.read_sensor("moisture3",0)]
 
-
-    #print(f"Sensor 1 moisture content = {moist_sensor1}")
-    if moist_sensor1 > WET_POINT:
-        pump1.dose(DOSE_SPEED, DOSE_TIME+0.4)
-
-    #print(f"Sensor 2 moisture content = {moist_sensor2}")
-    if moist_sensor2 > WET_POINT:
-        pump2.dose(DOSE_SPEED, DOSE_TIME)
-
-    #print(f"Sensor 3 moisture content = {moist_sensor3}")
-    if moist_sensor3 > WET_POINT:
-        pump3.dose(DOSE_SPEED, DOSE_TIME+0.6)
-
+    watering = {1:bool,2:bool,3:bool}
+    water_amount = {1:int,2:int,3:int}
+    
+    i=0
+    pump=0
+    #When the moisture level reaches the DRY_POINT iterate through each pump, pumping 10ml of water until the WET_POINT is reached.
+    for x in moist_sensor:
+        i+=1
+        if x >= DRY_POINT or watering[i] == 1:
+            if x >= WET_POINT:
+                watering[i] = 1
+                print(f"Sensor "+str(i)+" moisture content = "+str(x))
+                pumps[pump].dose(DOSE_SPEED, DOSE_TIME+PUMP_SETTINGS["pump"+str(i)+"_fine_calibration"])
+                water_amount[i] += 10
+            else:
+                print(f"Sensor "+str(i)+" moisture content = "+str(x)+" Done Watering!")
+                watering[i] = 0
+        print(f"Sensor "+str(i)+" moisture content = "+str(x)+" Don't Water!")
+        pump+=1
+    
+    #return the amount of water each plant was provided during the watering cycle. 
+    return water_amount
 
 
 while True:
 
         need_water()
         time.sleep(1)
-        ReadSensors.read_all(1)
-        time.sleep(300)
+        #ReadSensors.read_all(1)
+        time.sleep(10)
 
     
     
