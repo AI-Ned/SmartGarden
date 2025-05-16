5b552af30014feb267342d1fabaabe3174820a9c
AI-Ned <ta.hill0@googlemail.com>
2025-05-16 01:00:50 +0100

    - Added Image settings for image compression and storage directory -updated main.py to add the camera class. -updated camera class to write image to file, and not database due to database limitations.

--- camera_service.py
+++ camera_service.py
@@ -1,9 +1,10 @@
 from picamera import PiCamera
 from datetime import datetime
 from io import BytesIO
-import base64
-import yaml
 from database_actions import DatabaseActions
+import os
+import yaml
+import zlib
 
 
 class CameraImage:
@@ -15,22 +16,32 @@ class CameraImage:
         
         IMAGE_NAME = IMAGE_SETTINGS["image_name"]
         FILE_TYPE = IMAGE_SETTINGS["file_type"]
+        DIRECTORY = IMAGE_SETTINGS["save_directory"]
+        COMPRESS = IMAGE_SETTINGS["compress_image"]
                
         current_datetime = datetime.now().strftime("%Y%m%d%H%M%S")
         pi_cam = PiCamera()
         pi_cam.resolution = (640, 480)
-        filename = str('"'+IMAGE_NAME+current_datetime+"."+FILE_TYPE+'"')
+        filename = str(IMAGE_NAME+current_datetime+"."+FILE_TYPE)
         stream = BytesIO()
-
+       
         try:
             pi_cam.capture(stream, "jpeg")
             stream.seek(0)
-            encoded_image = base64.b64encode(stream.read())
-            data = "image_name="+filename+",image="+str(encoded_image)
-            #print(str(encoded_image) + "rewinded")
-            DatabaseActions.database_write("images","camera=PiZeroStandardCamera",data)
+            write_image = stream.read()
+ 
+            if COMPRESS:
+                compressor = zlib.compressobj(level=9,method=zlib.DEFLATED, wbits=zlib.MAX_WBITS,memLevel=9,strategy=zlib.Z_DEFAULT_STRATEGY)
+                filename = str(IMAGE_NAME+current_datetime)
+                with open(os.path.join(DIRECTORY, filename), "wb") as file:
+                    file.write(compressor.compress(write_image))
+            else:
+                filename = str(IMAGE_NAME+current_datetime+"."+FILE_TYPE)
+                with open(os.path.join(DIRECTORY, filename), "wb") as file:
+                    file.write(write_image)
+
         finally:
-            pi_cam.close() 
+            pi_cam.close()
             stream.flush()
             
 
--- play.py
+++
@@ -1,8 +0,0 @@
-import re
-from datetime import datetime
-reg = r"\s+(?=\d{2}(?:\d{2})?-\d{1,2}-\d{1,2}\b)"
-
-
-current_datetime = datetime.now().strftime("%Y%m%d%H%M%S")
-
-print(current_datetime)
